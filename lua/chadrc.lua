5af1334b635596d2b361077030e43e774c6178a9
vasudua <vasu.dua@instabase.com>
2024-07-18 20:10:14 +0530

    Added pyright, ruff & mypy to lsdpconfig

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.ui = {
-	theme = "vscode_dark",
+	theme = "catppuccin",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,16 +14,32 @@ return {
       -- calling `setup` is optional for customization
       require("fzf-lua").setup({})
     end
-  }
-
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "mypy",
+        "pyright",
+        "ruff",
+      },
+    },
+  },
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "configs.null-ls"
+    end
+  }
   --
   -- {
   -- 	"williamboman/mason.nvim",
